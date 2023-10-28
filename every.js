ec015ebff17baa29bf66fd5d2ae55097832df914
Maurice <mauriceglenn2@gmail.com>
2023-10-28 17:46:51 -0500

    learning every

---
+++ every.js
@@ -0,0 +1,7 @@
+//Use every() method, can only be used on arrays
+//Use every() method, can only be used on arrays
+//this is inefficent 
+// const palindrome = (str) => {
+//     return str.split('').every((char, index) =>(
+//         char === str[str.length - 1 - index]))
+// };
