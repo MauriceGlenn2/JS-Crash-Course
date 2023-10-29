6fcc3c871abba73ac81d671a27d00f8c4e7076c2
Maurice <mauriceglenn2@gmail.com>
2023-10-29 11:18:00 -0500

    Reverse Int

---
+++ reverseInt.js
@@ -0,0 +1,35 @@
+/** Reverse the ordering of the numbers of a given integer.
+ *
+ * @example
+ * reverseInt(4321) === 1234
+ * reverseInt(100) === 1
+ * reverseInt(-641) === -146
+ * reverseInt(-50) === -5
+ */
+
+const reverseInteger = (int) => {
+  let reversed = parseInt(int.toString().split("").reverse().join(""));
+  if (int < 0) {
+    return reversed * -1;
+  }
+  return reversed;
+};
+
+//better way if ternary 
+const reverseInteger = (int) => {
+  let reversed = parseInt(int.toString().split("").reverse().join(""));
+  return int < 0 ? reversed * -1 : reversed;
+};
+
+//cleaning up code
+const reverseInteger = (int) => {
+  const reversed = int.toString().split("").reverse().join("");
+  return parseInt(reversed) * Math.sign(int);
+};
+
+
+
+module.exports = reverseInteger;
+
+
+
